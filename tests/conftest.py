9f115a32d0a39593c372a0700ea275f243a6a844
Dourado <a@s.com>
2022-05-06 10:18:33 -0300

    [refactor] Update the deprecated action

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,21 @@
 import pytest
-import selenium.webdriver
-import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from pytest_bdd import given, then, parsers
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope='session')
 def driver():
-    opts = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     return driver
 
 
-def pytest_bdd_before_scenario(request, feature, scenario):
+def pytest_bdd_before_scenario(request):
     driver = request.getfixturevalue('driver')
     driver.maximize_window()
 
-# def pytest_bdd_after_scenario():
-#     # Quit the WebDriver instance for the teardown
-#     driver.quit()
+
+def pytest_bdd_after_scenario(request):
+    # Quit the WebDriver instance for the teardown
+    driver = request.getfixturevalue('driver')
+    driver.quit()
